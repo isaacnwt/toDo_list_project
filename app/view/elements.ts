44278f16ca259bfeb0d7e9c657cb12fa055e43ba
isaacnwt <isaacnewton02@hotmail.com>
2023-01-12 23:22:07 -0300

    Elements convertido para TS

---
+++ app/view/elements.ts
@@ -0,0 +1,5 @@
+export const elements = {
+    input: document.querySelector(".input_adicionar_tarefa"),
+    addButton: document.querySelector(".botao_adicionar_tarefa"),
+    tasksContainer: document.querySelector("#tasks-container")
+}
